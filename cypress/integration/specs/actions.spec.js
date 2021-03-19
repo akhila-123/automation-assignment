fb8ca97a2370b26d7f94060544bf9a3ec8f9f2b6
Akhila Bandiki <Akhila.Bandiki@ATMECS.CORP>
2021-03-19 21:43:40 +0530

    added new tests

---
+++ cypress/integration/specs/actions.spec.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+context('Verify all the functinalities of todos', () => {
+  beforeEach(()=>{
+    cy.log("=======Execution of it block starts here======");
+  })
+
+  afterEach(()=>{
+    cy.log("=======Execution of it block ends here======");
+  })
+
+  it('Verify create functionality of todos', () => {
+    cy.get('input').clear().type('Schedule appointment on Monday').type('{ENTER}');
+    cy.get('label').should('have.text','Schedule appointment on Monday');
+    cy.get('.footer').should('have.text','\n\t\t1 item left\n\t\t\n\t');
+  })
+
+  it('Verify edit functionality of todos', () => {
+    cy.get('label').dblclick();
+    cy.get('input.edit').focus().type(' 2 pm').type('{ENTER}');
+    cy.get('label').should('have.text','Schedule appointment on Monday 2 pm');
+    cy.get('.footer').should('have.text','\n\t\t1 item left\n\t\t\n\t');
+  })
+
+  it('Verify complete functionality of todos', () => {
+    cy.get('input.toggle[type="checkbox"]').check();
+    cy.get('.footer').should('have.text','\n\t\t0 items left\n\t\tClear completed\n\t');
+    cy.get('.clear-completed').should('be.visible').and('have.text','Clear completed');
+  })
+
+  it('Verify delete functionality of todos', () => {
+    cy.get('.clear-completed').click();
+    cy.get('input.toggle[type="checkbox"]').should('not.exist');
+  })
+
+})
