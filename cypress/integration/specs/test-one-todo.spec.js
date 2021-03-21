45f9c12bd00a24cac3584d81426ef5a7072b3f04
Akhila Bandiki <Akhila.Bandiki@ATMECS.CORP>
2021-03-21 17:02:32 +0530

    added some more tests

--- cypress/integration/specs/test-one-todo.spec.js
+++ cypress/integration/specs/test-one-todo.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-context("Verify all the functinalities of todos", () => {
+context("Verify for single todo", () => {
   beforeEach(() => {
     cy.log("=======Execution of it block starts here======");
   });
@@ -9,7 +9,7 @@ context("Verify all the functinalities of todos", () => {
     cy.log("=======Execution of it block ends here======");
   });
 
-  it("Verify create functionality of todos", () => {
+  it("Verify create functionality", () => {
     cy.get("input")
       .clear()
       .type("Schedule appointment on Monday")
@@ -18,22 +18,25 @@ context("Verify all the functinalities of todos", () => {
     cy.get(".footer").should("have.text", "\n\t\t1 item left\n\t\t\n\t");
   });
 
-  it("Verify edit functionality of todos", () => {
+  it("Verify edit functionality", () => {
     cy.get("label").dblclick();
     cy.get("input.edit").focus().type(" 2 pm").type("{ENTER}");
     cy.get("label").should("have.text", "Schedule appointment on Monday 2 pm");
     cy.get(".footer").should("have.text", "\n\t\t1 item left\n\t\t\n\t");
   });
 
-  it("Verify complete functionality of todos", () => {
+  it("Verify complete functionality", () => {
     cy.get('input.toggle[type="checkbox"]').check();
-    cy.get(".footer").should("have.text","\n\t\t0 items left\n\t\tClear completed\n\t");
+    cy.get(".footer").should(
+      "have.text",
+      "\n\t\t0 items left\n\t\tClear completed\n\t"
+    );
     cy.get(".clear-completed")
       .should("be.visible")
       .and("have.text", "Clear completed");
   });
 
-  it("Verify delete functionality of todos", () => {
+  it("Verify delete functionality", () => {
     cy.get(".clear-completed").click();
     cy.get('input.toggle[type="checkbox"]').should("not.exist");
   });
